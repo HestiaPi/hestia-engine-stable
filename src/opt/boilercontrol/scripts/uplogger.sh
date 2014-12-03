f0bda5c77c109d3d841aa64b908ef7625943a506
gulliverrr <github.com@koumanos.com>
2014-12-04 00:25:51 +0200

    Improved logging metrics

--- src/opt/boilercontrol/scripts/uplogger.sh
+++ src/opt/boilercontrol/scripts/uplogger.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+
 DATE=$(date +%Y%m%d)
 MAC=$(ip addr show eth0 | grep link | awk '{print $2}')
 NAME=${MAC}_${DATE}
+cat /opt/boilercontrol/version > /var/log/stats
+cat /var/www/version >> /var/log/stats
+uptime >> /var/log/stats
+df -h / | grep dev >> /var/log/stats
+sed "s/\(...\)$/.\1°C/" < /sys/class/thermal/thermal_zone0/temp >> /var/log/sta$
 find /var/log/ -mtime -1 | xargs tar --no-recursion -czf alllogs.tgz
 mv alllogs.tgz $NAME.tgz
 ftp -i -n isharemyprojects.com <<EOF
