19778b4a9e8740e924ee118c381caccf82920b12
gulliverrr <github.com@koumanos.com>
2014-12-10 19:25:30 +0200

    Update compile.sh

--- src/opt/boilercontrol/compile.sh
+++ src/opt/boilercontrol/compile.sh
@@ -5,7 +5,6 @@ if (( `id -u` != 0 )); then { MSG="Sorry, must be root.  Exiting..."; echo $MSG;
 echo "Touching source files"
 find . -name '*.java' | xargs touch
 LIBS=libs/raspberrygpio.jar:/home/pi/java:libs/commonsio24.jar:libs/mysql-connector-java-5.1.21-bin.jar:libs/framboos-0.0.1.jar:libs/pi4j-core.jar:./
-echo "Compiling"
+echo "Compiling..."
 javac -classpath $LIBS uk/co/jaynne/BoilerControl.java
-#echo "Done, running"
-#java -cp $LIBS uk/co/jaynne/BoilerControl
+echo "Compilation completed!"
